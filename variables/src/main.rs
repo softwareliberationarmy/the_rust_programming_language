8b3fd00ee5cf38914202306b0b3c249c25bee78a
Kerry Patrick <softwareliberationarmy@gmail.com>
2025-07-16 07:25:41 -0500

    adding tuples to variable examples

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,14 @@ fn main() {
     println!("The value of x is {x}");
     x = 6;
     println!("The value of x is {x}");
+
+    // tuples
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (_x, y, z) = tup;
+    println!("The value of y is {y}");
+    println!("The value of z is {z}");
+    // tup.0 = 5;
+
+    let unit_tuple: () = ();
+    println!("The value of unit_tuple is {unit_tuple:?}");
 }
